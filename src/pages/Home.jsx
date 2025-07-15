94518b3f33a9e01afcc3c1a6ebe26587aaa23082
Devika <dileepdevika3@gmail.com>
2025-07-15 06:16:25 +0530

    feat(): Add about and skill section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import ThemeToggle from '@/components/ThemeToggle'
 import StarBackground from '@/components/StarBackground'
 import Navbar from '@/components/Navbar'
 import HeroSection from '@/components/HeroSection'
+import AboutSection from '@/components/AboutSection'
+import SkillsSection from '@/components/SkillsSection'
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -18,6 +20,8 @@ export default function Home() {
         {/* Main Content */}
         <main>
             <HeroSection/>
+            <AboutSection/>
+            <SkillsSection/>
         </main>
 
         {/* Footer */}
